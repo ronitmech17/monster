bd8910a7a00fa57a55c60c54b507667cdea528c0
ronitmech17 <44895080+ronitmech17@users.noreply.github.com>
2019-06-30 23:45:32 +0530

    Added baseline

---
+++ MonsterTradingTerminal/MonsterTradingTerminal/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MonsterTradingTerminal
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbScripts = new System.Windows.Forms.ListBox();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.Script = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TotalTrades = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.WinTrades = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.WinPercent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StopLossHit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TakeProfitHit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProfitLoss = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbStrategy = new System.Windows.Forms.ListBox();
+            this.btnBacktest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbScripts
+            // 
+            this.lbScripts.FormattingEnabled = true;
+            this.lbScripts.Items.AddRange(new object[] {
+            "NIFTY",
+            "TATAMOTORS"});
+            this.lbScripts.Location = new System.Drawing.Point(7, 31);
+            this.lbScripts.Name = "lbScripts";
+            this.lbScripts.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbScripts.Size = new System.Drawing.Size(153, 368);
+            this.lbScripts.TabIndex = 0;
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Script,
+            this.TotalTrades,
+            this.WinTrades,
+            this.WinPercent,
+            this.StopLossHit,
+            this.TakeProfitHit,
+            this.ProfitLoss});
+            this.dgvResults.Location = new System.Drawing.Point(166, 31);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.Size = new System.Drawing.Size(744, 368);
+            this.dgvResults.TabIndex = 1;
+            // 
+            // Script
+            // 
+            this.Script.HeaderText = "Script";
+            this.Script.Name = "Script";
+            // 
+            // TotalTrades
+            // 
+            this.TotalTrades.HeaderText = "Total Trades";
+            this.TotalTrades.Name = "TotalTrades";
+            // 
+            // WinTrades
+            // 
+            this.WinTrades.HeaderText = "Win Trades";
+            this.WinTrades.Name = "WinTrades";
+            // 
+            // WinPercent
+            // 
+            this.WinPercent.HeaderText = "Win %";
+            this.WinPercent.Name = "WinPercent";
+            // 
+            // StopLossHit
+            // 
+            this.StopLossHit.HeaderText = "Stop Loss Hit";
+            this.StopLossHit.Name = "StopLossHit";
+            // 
+            // TakeProfitHit
+            // 
+            this.TakeProfitHit.HeaderText = "Take Profit Hit";
+            this.TakeProfitHit.Name = "TakeProfitHit";
+            // 
+            // ProfitLoss
+            // 
+            this.ProfitLoss.HeaderText = "Profit/Loss";
+            this.ProfitLoss.Name = "ProfitLoss";
+            // 
+            // lbStrategy
+            // 
+            this.lbStrategy.FormattingEnabled = true;
+            this.lbStrategy.Items.AddRange(new object[] {
+            "Fake Crossover",
+            "SMA50 Strategy",
+            "Stock Pro Strategy"});
+            this.lbStrategy.Location = new System.Drawing.Point(916, 31);
+            this.lbStrategy.Name = "lbStrategy";
+            this.lbStrategy.Size = new System.Drawing.Size(153, 368);
+            this.lbStrategy.TabIndex = 2;
+            // 
+            // btnBacktest
+            // 
+            this.btnBacktest.Location = new System.Drawing.Point(445, 419);
+            this.btnBacktest.Name = "btnBacktest";
+            this.btnBacktest.Size = new System.Drawing.Size(160, 23);
+            this.btnBacktest.TabIndex = 3;
+            this.btnBacktest.Text = "Backtest";
+            this.btnBacktest.UseVisualStyleBackColor = true;
+            this.btnBacktest.Click += new System.EventHandler(this.BtnBacktest_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 452);
+            this.Controls.Add(this.btnBacktest);
+            this.Controls.Add(this.lbStrategy);
+            this.Controls.Add(this.dgvResults);
+            this.Controls.Add(this.lbScripts);
+            this.Name = "frmMain";
+            this.Text = "Monster Trading Terminal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbScripts;
+        private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.ListBox lbStrategy;
+        private System.Windows.Forms.Button btnBacktest;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Script;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalTrades;
+        private System.Windows.Forms.DataGridViewTextBoxColumn WinTrades;
+        private System.Windows.Forms.DataGridViewTextBoxColumn WinPercent;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StopLossHit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TakeProfitHit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProfitLoss;
+    }
+}
+
---
+++ MonsterTradingTerminal/MonsterTradingTerminal/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trady;
+using Trady.Analysis.Indicator;
+using Trady.Core.Infrastructure;
+using Trady.Importer.Csv;
+using Trady.Importer.Yahoo;
+using Trady.Analysis.Backtest;
+using Trady.Analysis;
+using Trady.Analysis.Extension;
+using Rule = Trady.Analysis.Rule;
+using System.IO;
+using Trady.Analysis.Infrastructure;
+
+namespace MonsterTradingTerminal
+{
+    // Implement your pattern by creating a static class for extending IndexedCandle class
+    public static class IndexedCandleExtension
+    {
+        public static bool IsOpenAboveSma(this IIndexedOhlcv ic, int periodCount = 50)
+           => ic.Get<SimpleMovingAverage>(periodCount)[ic.Index].Tick.IsTrue(t => ic.Open > t);
+
+        public static bool IsOpenBelowSma(this IIndexedOhlcv ic, int periodCount = 50)
+            => ic.Get<SimpleMovingAverage>(periodCount)[ic.Index].Tick.IsTrue(t => ic.Open < t);
+
+        public static bool IsSma10BullishCrossSma50(this IndexedCandle ic)
+            => ic.Get<SimpleMovingAverage>(10, 50).ComputeNeighbour(ic.Index).IsTrue(
+                (prev, current, next) => prev.Tick.IsNegative() && current.Tick.IsPositive());
+
+        public static bool IsSma10BearishCrossSma50(this IndexedCandle ic)
+            => ic.Get<SimpleMovingAverage>(10, 50).ComputeNeighbour(ic.Index).IsTrue(
+                (prev, current, next) => prev.Tick.IsPositive() && current.Tick.IsNegative());
+
+        public static bool IsSma10BullishCrossSma200(this IndexedCandle ic)
+            => ic.Get<SimpleMovingAverage>(10, 200).ComputeNeighbour(ic.Index).IsTrue(
+                (prev, current, next) => prev.Tick.IsPositive() && current.Tick.IsNegative());
+
+        public static bool IsSma10BearishCrossSma200(this IndexedCandle ic)
+            => ic.Get<SimpleMovingAverage>(10, 200).ComputeNeighbour(ic.Index).IsTrue(
+                (prev, current, next) => prev.Tick.IsPositive() && current.Tick.IsNegative());
+
+        public static bool IsSma50BullishCrossSma200(this IndexedCandle ic)
+            => ic.Get<SimpleMovingAverage>(50, 200).ComputeNeighbour(ic.Index).IsTrue(
+                (prev, current, next) => prev.Tick.IsPositive() && current.Tick.IsNegative());
+
+        public static bool IsSma50BearishCrossSma200(this IndexedCandle ic)
+            => ic.Get<SimpleMovingAverage>(50, 200).ComputeNeighbour(ic.Index).IsTrue(
+                (prev, current, next) => prev.Tick.IsPositive() && current.Tick.IsNegative());
+
+        public static bool IsRsiOverbought(this IIndexedOhlcv ic, int periodCount = 14)
+          => ic.Get<RelativeStrengthIndex>(periodCount)[ic.Index].Tick.IsTrue(t => t >= 60);
+
+        public static bool IsRsiOversold(this IIndexedOhlcv ic, int periodCount = 14)
+            => ic.Get<RelativeStrengthIndex>(periodCount)[ic.Index].Tick.IsTrue(t => t <= 40);
+    }
+
+    public partial class frmMain : Form
+    {
+        private const string logPath = "backtest.txt";
+
+        private Predicate<IIndexedOhlcv> buyRule = null;
+        private Predicate<IIndexedOhlcv> sellRule = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public async Task<IEnumerable<IOhlcv>> ImportIOhlcvDatasAsync(string script)
+        {
+            var csvImporter = new CsvImporter(@"Data/" + script + ".csv", CultureInfo.GetCultureInfo("en-US"));
+            return await csvImporter.ImportAsync(script);
+        }
+        private void stockProStrategy()
+        {        
+            //buyRule = Rule.Create(c => IndexedCandleExtension.IsSma10BullishCrossSma50()).And(c => c.IsSma10BullishCrossSma50());
+            //sellRule = Rule.Create(c => c.IsBelowSma(50));
+        }
+
+        private void sma50Strategy()
+        {
+            buyRule = Rule.Create(c => c.IsAboveSma(50));
+            sellRule = Rule.Create(c => c.IsBelowSma(50));
+        }
+
+        private async Task<Result> ExecuteTrade(string script)
+        {
+            // Import your candles
+            var fullIOhlcvDatas = await ImportIOhlcvDatasAsync(script);
+            var candles = fullIOhlcvDatas.Where(c => c.DateTime > new DateTime(2017, 1, 1)).ToList();
+
+            buyRule = null;
+            sellRule = null;
+            // Build buy rule & sell rule based on various patterns
+            switch (lbStrategy.SelectedItem.ToString())
+            {
+                case ("SMA50 Strategy"):
+                    {
+                        sma50Strategy();
+                        break;
+                    }
+                case ("Stock Pro Strategy"):
+                    {
+                        stockProStrategy();
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+
+
+            if (buyRule == null || sellRule == null)
+            {
+                return null;
+            }
+
+            // Create portfolio instance by using PortfolioBuilder
+            var runner = new Builder()
+            .Add(candles)
+            .Buy(buyRule)
+            .Sell(sellRule)
+            .Build();
+
+            // Start backtesting with the portfolio
+            runner.OnBought += Backtest_OnBought;
+            runner.OnSold += Backtest_Onsold;
+            return await runner.RunAsync(10000);
+        }
+
+        private async void BtnBacktest_Click(object sender, EventArgs e)
+        {
+            Result result = null;
+            var script = "";
+
+            var scripts = this.lbScripts.SelectedItems;
+
+            dgvResults.Rows.Clear();
+            for (var i = 0; i < scripts.Count; i++)
+            {
+                script = scripts[i].ToString();
+                result = await ExecuteTrade(script);
+
+                if (result != null)
+                {
+                    // Get backtest result for the portfolio
+                    dgvResults.Rows.Add();
+                    dgvResults.Rows[i].Cells[0].Value = script;
+                    dgvResults.Rows[i].Cells[1].Value = result.Transactions.Count().ToString();
+                    dgvResults.Rows[i].Cells[6].Value = result.TotalCorrectedProfitLoss;
+                }
+                else
+                {
+                    MessageBox.Show("Fatal Error occured");
+                    dgvResults.Rows.Clear();
+                }
+
+            }
+        }
+
+        private void Backtest_Onsold(IEnumerable<IOhlcv> candles, int index, DateTimeOffset dateTime, decimal sellPrice, decimal quantity, decimal absCashFlow, decimal currentCashAmount, decimal plRatio)
+        {
+            File.AppendAllLines(logPath, new[] { $"{index}({dateTime:yyyyMMdd}), Sell {candles.GetHashCode()}@{sellPrice} * {quantity}: {absCashFlow}, plRatio: {plRatio * 100:0.##}%, currentCashAmount: {currentCashAmount}" });
+        }
+
+        private void Backtest_OnBought(IEnumerable<IOhlcv> candles, int index, DateTimeOffset dateTime, decimal buyPrice, decimal quantity, decimal absCashFlow, decimal currentCashAmount)
+        {
+            File.AppendAllLines(logPath, new[] { $"{index}({dateTime:yyyyMMdd}), Buy {candles.GetHashCode()}@{buyPrice} * {quantity}: {absCashFlow}, currentCashAmount: {currentCashAmount}" });
+        }
+    }
+}
